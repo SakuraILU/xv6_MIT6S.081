11bb75ad40178d6cbca5ffe6d2078ef72cbeb73f
SakuraILU <lkyhaq@gmail.com>
2022-09-20 15:20:03 +0800

    lab3 is finished 	add isolate kernel pagetable for each process 	add kvmcopy(), kvmdealloc(), kvmfree(), kvmmapproc(), kvmpaproc() and etc to support below modification 	modified userinit(), fork(), exec(), growproc(), freeproc()

--- kernel/exec.c
+++ kernel/exec.c
@@ -9,12 +9,11 @@
 
 static int loadseg(pde_t *pgdir, uint64 addr, struct inode *ip, uint offset, uint sz);
 
-int
-exec(char *path, char **argv)
+int exec(char *path, char **argv)
 {
   char *s, *last;
   int i, off;
-  uint64 argc, sz = 0, sp, ustack[MAXARG+1], stackbase;
+  uint64 argc, sz = 0, sp, ustack[MAXARG + 1], stackbase;
   struct elfhdr elf;
   struct inode *ip;
   struct proghdr ph;
@@ -22,39 +21,48 @@ exec(char *path, char **argv)
   struct proc *p = myproc();
 
   begin_op();
-
-  if((ip = namei(path)) == 0){
+  if ((ip = namei(path)) == 0)
+  {
     end_op();
     return -1;
   }
   ilock(ip);
 
   // Check ELF header
-  if(readi(ip, 0, (uint64)&elf, 0, sizeof(elf)) != sizeof(elf))
+  if (readi(ip, 0, (uint64)&elf, 0, sizeof(elf)) != sizeof(elf))
     goto bad;
-  if(elf.magic != ELF_MAGIC)
+  if (elf.magic != ELF_MAGIC)
     goto bad;
 
-  if((pagetable = proc_pagetable(p)) == 0)
+  // create a basic user pagetable with trapoline and trapoframe and without user memory
+  if ((pagetable = proc_pagetable(p)) == 0)
     goto bad;
 
   // Load program into memory.
-  for(i=0, off=elf.phoff; i<elf.phnum; i++, off+=sizeof(ph)){
-    if(readi(ip, 0, (uint64)&ph, off, sizeof(ph)) != sizeof(ph))
+  for (i = 0, off = elf.phoff; i < elf.phnum; i++, off += sizeof(ph))
+  {
+    if (readi(ip, 0, (uint64)&ph, off, sizeof(ph)) != sizeof(ph))
       goto bad;
-    if(ph.type != ELF_PROG_LOAD)
+    // if ph.tyeo is ELF_PROG_LOAD, the program segment refered by this ph should be loaded
+    if (ph.type != ELF_PROG_LOAD)
       continue;
-    if(ph.memsz < ph.filesz)
+    // some parts like bss is not stored in elf, bcz bytes are all 0, just kalloc is enough..
+    // so filesz is definitely <= memsz.... this is an example of defensive programming
+    if (ph.memsz < ph.filesz)
       goto bad;
-    if(ph.vaddr + ph.memsz < ph.vaddr)
+    // deal with overflow?
+    if (ph.vaddr + ph.memsz < ph.vaddr)
       goto bad;
+
+    // malloc pagetable [vaddr, vaddr+memsz]
     uint64 sz1;
-    if((sz1 = uvmalloc(pagetable, sz, ph.vaddr + ph.memsz)) == 0)
+    if ((sz1 = uvmalloc(pagetable, sz, ph.vaddr + ph.memsz)) == 0)
       goto bad;
     sz = sz1;
-    if(ph.vaddr % PGSIZE != 0)
+    if (ph.vaddr % PGSIZE != 0)
       goto bad;
-    if(loadseg(pagetable, ph.vaddr, ip, ph.off, ph.filesz) < 0)
+    // load this program segment into memory and map pagetable
+    if (loadseg(pagetable, ph.vaddr, ip, ph.off, ph.filesz) < 0)
       goto bad;
   }
   iunlockput(ip);
@@ -63,68 +71,83 @@ exec(char *path, char **argv)
 
   p = myproc();
   uint64 oldsz = p->sz;
-
   // Allocate two pages at the next page boundary.
   // Use the second as the user stack.
   sz = PGROUNDUP(sz);
   uint64 sz1;
-  if((sz1 = uvmalloc(pagetable, sz, sz + 2*PGSIZE)) == 0)
+  if ((sz1 = uvmalloc(pagetable, sz, sz + 2 * PGSIZE)) == 0)
     goto bad;
   sz = sz1;
-  uvmclear(pagetable, sz-2*PGSIZE);
+  uvmclear(pagetable, sz - 2 * PGSIZE); // there is a guard page below stakc (not kalloced and mapped, invalid to r/w/x)
   sp = sz;
   stackbase = sp - PGSIZE;
 
+  // if proc's user pgtbl image will override kernel pgtbl, it's a bad proc....fail to exec
+  if (sz >= PLIC)
+    goto bad;
+
   // Push argument strings, prepare rest of stack in ustack.
-  for(argc = 0; argv[argc]; argc++) {
-    if(argc >= MAXARG)
+  // ustack[] stores argv[] byte stream temporarily
+  for (argc = 0; argv[argc]; argc++)
+  {
+    if (argc >= MAXARG)
       goto bad;
     sp -= strlen(argv[argc]) + 1;
     sp -= sp % 16; // riscv sp must be 16-byte aligned
-    if(sp < stackbase)
+    if (sp < stackbase)
       goto bad;
-    if(copyout(pagetable, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
+    if (copyout(pagetable, sp, argv[argc], strlen(argv[argc]) + 1) < 0)
       goto bad;
     ustack[argc] = sp;
   }
-  ustack[argc] = 0;
+  ustack[argc] = 0; // the end of argv[] is null
 
   // push the array of argv[] pointers.
-  sp -= (argc+1) * sizeof(uint64);
+  sp -= (argc + 1) * sizeof(uint64);
   sp -= sp % 16;
-  if(sp < stackbase)
+  if (sp < stackbase)
     goto bad;
-  if(copyout(pagetable, sp, (char *)ustack, (argc+1)*sizeof(uint64)) < 0)
+  if (copyout(pagetable, sp, (char *)ustack, (argc + 1) * sizeof(uint64)) < 0)
     goto bad;
 
+  // printf("free kernel pagesize %p\n", oldsz);
   // arguments to user main(argc, argv)
   // argc is returned via the system call return
   // value, which goes in a0.
   p->trapframe->a1 = sp;
 
-  // Save program name for debugging.
-  for(last=s=path; *s; s++)
-    if(*s == '/')
-      last = s+1;
+  // Save program name for debugging. only store last name in the path (basename)
+  for (last = s = path; *s; s++)
+    if (*s == '/')
+      last = s + 1;
   safestrcpy(p->name, last, sizeof(p->name));
-    
+
   // Commit to the user image.
   oldpagetable = p->pagetable;
   p->pagetable = pagetable;
   p->sz = sz;
-  p->trapframe->epc = elf.entry;  // initial program counter = main
-  p->trapframe->sp = sp; // initial stack pointer
+  p->trapframe->epc = elf.entry; // initial program counter = main
+  p->trapframe->sp = sp;         // initial stack pointer
+  // free proc's old pagetable (and related physical pages)
+  // and kernel_pagetable's user virtual memory part (shadow free...will not free physical memory again)
   proc_freepagetable(oldpagetable, oldsz);
+  kvmdealloc(p->kernel_pagetable, oldsz, 0);
+  kvmcopy(p->pagetable, p->kernel_pagetable, 0, p->sz); // shadow copy new user image to kernel_pagetable
+
+  if (p->pagetable)
+    vmprintf(p->pagetable);
 
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
- bad:
-  if(pagetable)
+bad:
+  if (pagetable)
     proc_freepagetable(pagetable, sz);
-  if(ip){
+  if (ip)
+  {
     iunlockput(ip);
     end_op();
   }
+  printf("exec out\n");
   return -1;
 }
 
@@ -138,20 +161,26 @@ loadseg(pagetable_t pagetable, uint64 va, struct inode *ip, uint offset, uint sz
   uint i, n;
   uint64 pa;
 
-  if((va % PGSIZE) != 0)
+  if ((va % PGSIZE) != 0)
     panic("loadseg: va must be page aligned");
 
-  for(i = 0; i < sz; i += PGSIZE){
+  for (i = 0; i < sz; i += PGSIZE)
+  {
+    // va + i points to the vaddr page to load into
+    // offset + i points to the elf offset to be loaded
+
+    // find vaddr[va+i]'s physical adress
     pa = walkaddr(pagetable, va + i);
-    if(pa == 0)
+    if (pa == 0)
       panic("loadseg: address should exist");
-    if(sz - i < PGSIZE)
-      n = sz - i;
+    if (sz - i < PGSIZE)
+      n = sz - i; // may be not a full page in the last program segment in elf
     else
       n = PGSIZE;
-    if(readi(ip, 0, (uint64)pa, offset+i, n) != n)
+    // write elf[offset + i] to physical address
+    if (readi(ip, 0, (uint64)pa, offset + i, n) != n)
       return -1;
   }
-  
+
   return 0;
 }
--- kernel/virtio_disk.c
+++ kernel/virtio_disk.c
@@ -16,16 +16,18 @@
 #include "fs.h"
 #include "buf.h"
 #include "virtio.h"
+#include "proc.h"
 
 // the address of virtio mmio register r.
 #define R(r) ((volatile uint32 *)(VIRTIO0 + (r)))
 
-static struct disk {
- // memory for virtio descriptors &c for queue 0.
- // this is a global instead of allocated because it must
- // be multiple contiguous pages, which kalloc()
- // doesn't support, and page aligned.
-  char pages[2*PGSIZE];
+static struct disk
+{
+  // memory for virtio descriptors &c for queue 0.
+  // this is a global instead of allocated because it must
+  // be multiple contiguous pages, which kalloc()
+  // doesn't support, and page aligned.
+  char pages[2 * PGSIZE];
   struct VRingDesc *desc;
   uint16 *avail;
   struct UsedArea *used;
@@ -37,29 +39,30 @@ static struct disk {
   // track info about in-flight operations,
   // for use when completion interrupt arrives.
   // indexed by first descriptor index of chain.
-  struct {
+  struct
+  {
     struct buf *b;
     char status;
   } info[NUM];
-  
-  struct spinlock vdisk_lock;
-  
-} __attribute__ ((aligned (PGSIZE))) disk;
 
-void
-virtio_disk_init(void)
+  struct spinlock vdisk_lock;
+
+} __attribute__((aligned(PGSIZE))) disk;
+
+void virtio_disk_init(void)
 {
   uint32 status = 0;
 
   initlock(&disk.vdisk_lock, "virtio_disk");
 
-  if(*R(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976 ||
-     *R(VIRTIO_MMIO_VERSION) != 1 ||
-     *R(VIRTIO_MMIO_DEVICE_ID) != 2 ||
-     *R(VIRTIO_MMIO_VENDOR_ID) != 0x554d4551){
+  if (*R(VIRTIO_MMIO_MAGIC_VALUE) != 0x74726976 ||
+      *R(VIRTIO_MMIO_VERSION) != 1 ||
+      *R(VIRTIO_MMIO_DEVICE_ID) != 2 ||
+      *R(VIRTIO_MMIO_VENDOR_ID) != 0x554d4551)
+  {
     panic("could not find virtio disk");
   }
-  
+
   status |= VIRTIO_CONFIG_S_ACKNOWLEDGE;
   *R(VIRTIO_MMIO_STATUS) = status;
 
@@ -90,9 +93,9 @@ virtio_disk_init(void)
   // initialize queue 0.
   *R(VIRTIO_MMIO_QUEUE_SEL) = 0;
   uint32 max = *R(VIRTIO_MMIO_QUEUE_NUM_MAX);
-  if(max == 0)
+  if (max == 0)
     panic("virtio disk has no queue 0");
-  if(max < NUM)
+  if (max < NUM)
     panic("virtio disk max queue too short");
   *R(VIRTIO_MMIO_QUEUE_NUM) = NUM;
   memset(disk.pages, 0, sizeof(disk.pages));
@@ -102,11 +105,11 @@ virtio_disk_init(void)
   // avail = pages + 0x40 -- 2 * uint16, then num * uint16
   // used = pages + 4096 -- 2 * uint16, then num * vRingUsedElem
 
-  disk.desc = (struct VRingDesc *) disk.pages;
-  disk.avail = (uint16*)(((char*)disk.desc) + NUM*sizeof(struct VRingDesc));
-  disk.used = (struct UsedArea *) (disk.pages + PGSIZE);
+  disk.desc = (struct VRingDesc *)disk.pages;
+  disk.avail = (uint16 *)(((char *)disk.desc) + NUM * sizeof(struct VRingDesc));
+  disk.used = (struct UsedArea *)(disk.pages + PGSIZE);
 
-  for(int i = 0; i < NUM; i++)
+  for (int i = 0; i < NUM; i++)
     disk.free[i] = 1;
 
   // plic.c and trap.c arrange for interrupts from VIRTIO0_IRQ.
@@ -116,8 +119,10 @@ virtio_disk_init(void)
 static int
 alloc_desc()
 {
-  for(int i = 0; i < NUM; i++){
-    if(disk.free[i]){
+  for (int i = 0; i < NUM; i++)
+  {
+    if (disk.free[i])
+    {
       disk.free[i] = 0;
       return i;
     }
@@ -129,9 +134,9 @@ alloc_desc()
 static void
 free_desc(int i)
 {
-  if(i >= NUM)
+  if (i >= NUM)
     panic("virtio_disk_intr 1");
-  if(disk.free[i])
+  if (disk.free[i])
     panic("virtio_disk_intr 2");
   disk.desc[i].addr = 0;
   disk.free[i] = 1;
@@ -142,9 +147,10 @@ free_desc(int i)
 static void
 free_chain(int i)
 {
-  while(1){
+  while (1)
+  {
     free_desc(i);
-    if(disk.desc[i].flags & VRING_DESC_F_NEXT)
+    if (disk.desc[i].flags & VRING_DESC_F_NEXT)
       i = disk.desc[i].next;
     else
       break;
@@ -154,10 +160,12 @@ free_chain(int i)
 static int
 alloc3_desc(int *idx)
 {
-  for(int i = 0; i < 3; i++){
+  for (int i = 0; i < 3; i++)
+  {
     idx[i] = alloc_desc();
-    if(idx[i] < 0){
-      for(int j = 0; j < i; j++)
+    if (idx[i] < 0)
+    {
+      for (int j = 0; j < i; j++)
         free_desc(idx[j]);
       return -1;
     }
@@ -165,8 +173,7 @@ alloc3_desc(int *idx)
   return 0;
 }
 
-void
-virtio_disk_rw(struct buf *b, int write)
+void virtio_disk_rw(struct buf *b, int write)
 {
   uint64 sector = b->blockno * (BSIZE / 512);
 
@@ -178,23 +185,26 @@ virtio_disk_rw(struct buf *b, int write)
 
   // allocate the three descriptors.
   int idx[3];
-  while(1){
-    if(alloc3_desc(idx) == 0) {
+  while (1)
+  {
+    if (alloc3_desc(idx) == 0)
+    {
       break;
     }
     sleep(&disk.free[0], &disk.vdisk_lock);
   }
-  
+
   // format the three descriptors.
   // qemu's virtio-blk.c reads them.
 
-  struct virtio_blk_outhdr {
+  struct virtio_blk_outhdr
+  {
     uint32 type;
     uint32 reserved;
     uint64 sector;
   } buf0;
 
-  if(write)
+  if (write)
     buf0.type = VIRTIO_BLK_T_OUT; // write the disk
   else
     buf0.type = VIRTIO_BLK_T_IN; // read the disk
@@ -203,14 +213,14 @@ virtio_disk_rw(struct buf *b, int write)
 
   // buf0 is on a kernel stack, which is not direct mapped,
   // thus the call to kvmpa().
-  disk.desc[idx[0]].addr = (uint64) kvmpa((uint64) &buf0);
+  disk.desc[idx[0]].addr = (uint64)kvmpaproc(myproc()->kernel_pagetable, (uint64)&buf0);
   disk.desc[idx[0]].len = sizeof(buf0);
   disk.desc[idx[0]].flags = VRING_DESC_F_NEXT;
   disk.desc[idx[0]].next = idx[1];
 
-  disk.desc[idx[1]].addr = (uint64) b->data;
+  disk.desc[idx[1]].addr = (uint64)b->data;
   disk.desc[idx[1]].len = BSIZE;
-  if(write)
+  if (write)
     disk.desc[idx[1]].flags = 0; // device reads b->data
   else
     disk.desc[idx[1]].flags = VRING_DESC_F_WRITE; // device writes b->data
@@ -218,7 +228,7 @@ virtio_disk_rw(struct buf *b, int write)
   disk.desc[idx[1]].next = idx[2];
 
   disk.info[idx[0]].status = 0;
-  disk.desc[idx[2]].addr = (uint64) &disk.info[idx[0]].status;
+  disk.desc[idx[2]].addr = (uint64)&disk.info[idx[0]].status;
   disk.desc[idx[2]].len = 1;
   disk.desc[idx[2]].flags = VRING_DESC_F_WRITE; // device writes the status
   disk.desc[idx[2]].next = 0;
@@ -238,7 +248,8 @@ virtio_disk_rw(struct buf *b, int write)
   *R(VIRTIO_MMIO_QUEUE_NOTIFY) = 0; // value is queue number
 
   // Wait for virtio_disk_intr() to say request has finished.
-  while(b->disk == 1) {
+  while (b->disk == 1)
+  {
     sleep(b, &disk.vdisk_lock);
   }
 
@@ -248,18 +259,18 @@ virtio_disk_rw(struct buf *b, int write)
   release(&disk.vdisk_lock);
 }
 
-void
-virtio_disk_intr()
+void virtio_disk_intr()
 {
   acquire(&disk.vdisk_lock);
 
-  while((disk.used_idx % NUM) != (disk.used->id % NUM)){
+  while ((disk.used_idx % NUM) != (disk.used->id % NUM))
+  {
     int id = disk.used->elems[disk.used_idx].id;
 
-    if(disk.info[id].status != 0)
+    if (disk.info[id].status != 0)
       panic("virtio_disk_intr status");
-    
-    disk.info[id].b->disk = 0;   // disk is done with buf
+
+    disk.info[id].b->disk = 0; // disk is done with buf
     wakeup(disk.info[id].b);
 
     disk.used_idx = (disk.used_idx + 1) % NUM;
